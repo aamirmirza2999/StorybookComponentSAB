fddb977f455e7a817a9697b35755db3e6b170b24
“nirosha” <nirosha@clayfin.com>
2024-07-17 16:06:02 +0530

    figma light and dark theme colors added

--- constants/Theme/DarkTheme.js
+++ constants/Theme/DarkTheme.js
@@ -1,4 +1,6 @@
 export const darkTheme = {
+
+
   primaryColor1: '#ffffff',
   SecondaryColor: '#db0011', 
   primaryColor3: "#eaeaea", 
@@ -8,5 +10,92 @@ export const darkTheme = {
   SecondaryColor1: '#ffffff',
   SecondaryColor2: "rgba(255,255,255,0.5)", 
   PrimaryButtonBgClr: "#db0011",
+
+
+   //PRIMARY COLORS:
+
+   primarybackground: "#1E2E42",
+   primaryblack:"#FFFFFF",
+   primaryblackstatic:"#000000",
+   primarygray:"#FFFFFF",
+   primaryred:"#FFFFFF",
+   primaryredstatic:"#DB0011",
+   primaryredtonal:"rgba(219, 0, 17, 0.1)",
+   primarywhite:"#FFFFFF",
+   primarywhitestatic:"#FFFFFF",
+   primaryinvert:"#000000",
+   primaryblacknav:"rgba(255, 255, 255, 0.6)",
+ 
+   // PRIMARY GREY COLORS:
+ 
+   primarygray100:"rgba(255, 255, 255, 0.6)",
+   primarygray90:"rgba(255, 255, 255, 0.5)",
+   primarygray80:"rgba(255, 255, 255, 0.45)",
+   primarygray70:"rgba(255, 255, 255, 0.4)",
+   primarygray60:"rgba(255, 255, 255, 0.35)",
+   primarygray50:"rgba(255, 255, 255, 0.3)",
+   primarygray40:"rgba(255, 255, 255, 0.25)",
+   primarygray30:"rgba(255, 255, 255, 0.2)",
+   primarygray20:"rgba(255, 255, 255, 0.15)",
+   primarygray10:"rgba(255, 255, 255, 0.1)",
+ 
+ 
+   //COMPLIMENTARY COLORS
+ 
+   complimentaryred1:"#E31E22",
+   complimentaryred2:"#BA1110",
+   complimentaryred3:"#730014",
+ 
+   // TEXT COLORS:
+ 
+   textblack:"rgba(255, 255, 255, 0.8)",
+   textgray:"rgba(255, 255, 255, 0.6)",
+   textgray2:"rgba(255, 255, 255, 0.3)",
+   textsilver:"rgba(255, 255, 255, 0.15)",
+   textwhite:"#000000",
+   textred:"#FF0014",
+ 
+   //STYLES COLORS:
+ 
+   stylesbgshades:"rgba(245, 245, 245, 0.15)",
+   stylesblockbg:"rgba(255, 255, 255, 0.6)",
+   stylesbackground2:"#0F1721",
+   stylessecondarygreen:"#00B8B5",
+   styleslightgray:"rgba(255, 255, 255, 0.1)",
+   stylesredpressed:"#E96670",
+   stylesblackpressed:"#666666",
+ 
+   //SECONDARY COLORS:
+ 
+   secondaryyellow:"#E8E012",
+   secondaryorange:"#E6B012",
+   secondarybluegreen:"#00847F",
+   secondaryblue:"#6699C2",
+   secondarypurple:"#7878B0",
+   secondarycoldgreen:"#A3BDAD",
+   secondarylightgreen:"rgba(163, 217, 99, 0.8)",
+ 
+   //EXTRA COLORS:
+    
+   extrasverylightgray:"rgba(255, 255, 255, 0.2)",
+   extrastransparent:"rgba(255, 255, 255, 0.0001)",
+ 
+   //RAG COLORS:
+ 
+   ragred:"#A8000B",
+   ragamber:"#FFBB33",
+   raggreen:"#00847F",
+   ragneutral:"#305A85",
+   ragredtint:"#F9F2F3",
+   ragambertint:"#FFF8EA",
+   raggreentint:"#E5F2F2",
+   ragneutraltint:"#EBEFF4",
+ 
+   //STROKE COLORS:
+ 
+   strokeredtint:"#E5B2B5",
+   strokeambertint:"#FFCF70",
+   strokegreentint:"#99CECC",
+   strokeneutraltint:"#ACBDCE"
   };
   
--- constants/Theme/LightTheme.js
+++ constants/Theme/LightTheme.js
@@ -8,5 +8,92 @@ export const lightTheme = {
   SecondaryColor1: '#ffffff', //secclr1
   SecondaryColor2: "rgba(255,255,255,0.5)", 
   PrimaryButtonBgClr: "#db0011",
+
+  //PRIMARY COLORS:
+
+  primarybackground: "#1E2E42",
+  primaryblack:"#000000",
+  primaryblackstatic:"#000000",
+  primarygray:"#D7D8D6",
+  primaryred:"#DB0011",
+  primaryredstatic:"#DB0011",
+  primaryredtonal:"rgba(219, 0, 17, 0.1)",
+  primarywhite:"#FFFFFF",
+  primarywhitestatic:"#FFFFFF",
+  primaryinvert:"#FFFFFF",
+  primaryblacknav:"#000000",
+
+  // PRIMARY GREY COLORS:
+
+  primarygray100:"#767676",
+  primarygray90:"rgba(118, 118, 118, 0.9)",
+  primarygray80:"rgba(118, 118, 118, 0.8)",
+  primarygray70:"rgba(118, 118, 118, 0.7)",
+  primarygray60:"rgba(118, 118, 118, 0.6)",
+  primarygray50:"rgba(118, 118, 118, 0.5)",
+  primarygray40:"rgba(118, 118, 118, 0.4)",
+  primarygray30:"rgba(118, 118, 118, 0.3)",
+  primarygray20:"rgba(118, 118, 118, 0.2)",
+  primarygray10:"rgba(118, 118, 118, 0.1)",
+
+
+  //COMPLIMENTARY COLORS
+
+  complimentaryred1:"#E31E22",
+  complimentaryred2:"#BA1110",
+  complimentaryred3:"#730014",
+
+  // TEXT COLORS:
+
+  textblack:"#333333",
+  textgray:"#767676",
+  textgray2:"#B2B2B2",
+  textsilver:"#D7D8D6",
+  textwhite:"#FFFFFF",
+  textred:"#DB0011",
+
+  //STYLES COLORS:
+
+  stylesbgshades:"rgba(245, 245, 245, 0.5)",
+  stylesblockbg:"#F2F3F4",
+  stylesbackground2:"#FBFCFE",
+  stylessecondarygreen:"#00B8B5",
+  styleslightgray:"#EEEEEE",
+  stylesredpressed:"#E96670",
+  stylesblackpressed:"#666666",
+
+  //SECONDARY COLORS:
+
+  secondaryyellow:"#E8E012",
+  secondaryorange:"#E6B012",
+  secondarybluegreen:"#00847F",
+  secondaryblue:"#6699C2",
+  secondarypurple:"#7878B0",
+  secondarycoldgreen:"#A3BDAD",
+  secondarylightgreen:"rgba(163, 217, 99, 0.8)",
+
+  //EXTRA COLORS:
+   
+  extrasverylightgray:"#FAFAFA",
+  extrastransparent:"rgba(255, 255, 255, 0.0001)",
+
+  //RAG COLORS:
+
+  ragred:"#A8000B",
+  ragamber:"#FFBB33",
+  raggreen:"#00847F",
+  ragneutral:"#305A85",
+  ragredtint:"#F9F2F3",
+  ragambertint:"#FFF8EA",
+  raggreentint:"#E5F2F2",
+  ragneutraltint:"#EBEFF4",
+
+  //STROKE COLORS:
+
+  strokeredtint:"#E5B2B5",
+  strokeambertint:"#FFCF70",
+  strokegreentint:"#99CECC",
+  strokeneutraltint:"#ACBDCE"
+
   };
   
