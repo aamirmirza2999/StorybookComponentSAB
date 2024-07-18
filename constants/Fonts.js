811ee1fa7c5bb3d03f61f2271c07b4e58f9a20d3
Kalidoss <kalidoss@clayfin.com>
2024-07-18 16:36:30 +0530

    Medium fonts added

--- constants/Fonts.js
+++ constants/Fonts.js
@@ -12,16 +12,16 @@
 // };
 
 export default {
-    // UniversNextforHSBC_Medium: 'UniversNextforHSBC-Medium',
-    Light_En: 'UniversNextforHSBC-Light',
-    Bold_En: 'UniversNextforHSBC-Bold',
-    Regular_En: 'UniversNextforHSBC-Regular',
-    Light_Ar: 'Univers Arabic forHSBC Light',
-    Bold_Ar: 'Univers Arabic forHSBC Bold',
-    Regular_Ar: 'Univers Arabic forHSBC',
+  // UniversNextforHSBC_Medium: 'UniversNextforHSBC-Medium',
+  Light_En: 'UniversNextforHSBC-Light',
+  Bold_En: 'UniversNextforHSBC-Bold',
+  Regular_En: 'UniversNextforHSBC-Regular',
+  Medium_En: 'UniversNextforHSBC-Medium',
+  Light_Ar: 'Univers Arabic forHSBC Light',
+  Bold_Ar: 'Univers Arabic forHSBC Bold',
+  Regular_Ar: 'Univers Arabic forHSBC',
+  Thin_Ar: 'Univers Arabic forHSBC Thin',
 };
-
-
 
 //reglar_en
 //medium
