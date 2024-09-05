f1cca402d7bef76d3baf82a68255c874da59d5e0
“arthi” <arthi@clayfin.com>
2024-09-05 17:37:34 +0530

    Home image issue

--- constants/SvgLocations.js
+++ constants/SvgLocations.js
@@ -46,7 +46,7 @@ export {default as Close} from '../assets/close.svg';
 export {default as Success} from '../assets/success.svg';
 export {default as Cancel} from '../assets/cancel.svg';
 export {default as Home} from '../assets/home.svg'
-export {default as HomeDark} from '../assets/homeDark.svg'
+export {default as HomeDark} from '../assets/HomeDark.svg'
 export {default as LiveChat} from '../assets/livechat.svg';
 
 export {default as Check} from '../assets/check.svg';
@@ -62,7 +62,7 @@ export {default as SearchIconDark} from '../assets/searchDark.svg';
 export {default as NotificationIcon} from '../assets/notifications.svg';
 export {default as NotificationIconDark} from '../assets/notificationsDark.svg';
 export { default as PaymentActive } from "../assets/PaymentDark.svg";
-export { default as HomeActive } from "../assets/homeDark.svg";
+export { default as HomeActive } from "../assets/HomeDark.svg";
 export { default as Transfer } from "../assets/Transfer.svg";
 export { default as Payment_Icon } from "../assets/Payment.svg";
 export { default as HomeTab } from "../assets/Home_light.svg";
