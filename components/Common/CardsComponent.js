19b89eb921d72cb9c91b0540e9311724e83047bc
“nirosha” <nirosha@clayfin.com>
2024-09-06 10:35:09 +0530

    web issue fixed.

--- components/Common/CardsComponent.js
+++ components/Common/CardsComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {  View, ImageBackground } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+// import LinearGradient from "react-native-linear-gradient";
 import TextComponent from './TextComponent';
 import * as Progress from "react-native-progress";
 import SvgIconList from '../../constants/SvgIconList';
