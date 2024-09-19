ad83bd3c2d5dc9798a7f46aaa7e77527668aafcd
“nirosha” <nirosha@clayfin.com>
2024-09-19 14:38:30 +0530

    Link button added in Header.

--- App.js
+++ App.js
@@ -94,7 +94,7 @@ function App() {
 
 let AppEntryPoint = App;
 
-if (!process.env.STORYBOOK_ENABLED) {
+if (process.env.STORYBOOK_ENABLED) {
   const AppEntry = require('./.ondevice').default;
   AppEntryPoint = () => (
     <ThemeProvider>
