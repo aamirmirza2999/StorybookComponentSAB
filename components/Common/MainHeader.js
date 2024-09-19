be147ce43c051f790fee072b5539987e75fc51a8
“nirosha” <nirosha@clayfin.com>
2024-09-19 14:12:13 +0530

    zoho issue fix

--- components/Common/MainHeader.js
+++ components/Common/MainHeader.js
@@ -145,7 +145,7 @@ const MainHeader = (props) => {
                     {props.AvatarIconReq ?
                     
                         <TouchableOpacity
-                            style={{  }}
+                            style={{ paddingBottom:actuatedNormalize(6) }}
                             onPress={props.AvatarIconfunc}>
                             <Avatarcomponent
                              avatarblack={props.avatarblack}
